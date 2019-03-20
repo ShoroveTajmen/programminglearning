a9ab30b2d8b4f3e820553991a1a5a876b45eeccf
ShoroveTajmen <sorovitajmen@gmail.com>
2019-03-20 19:19:13 +0600

    maximum number

---
+++ march/max_num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("enter two numbers:");
+    scanf("%d%d",&num1,&num2);
+    if(num1 > num2)
+    {
+        printf("%d is meximum",num1);
+    }
+    else
+    {
+      printf("%d is meximum",num2);  /* code */
+    }
+    return 0;
+    
+    
+}
